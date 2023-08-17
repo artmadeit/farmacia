d6bc6b303be94ad772e6bddcd3a11dfecb79d862
artmadeit <this.arthur.email@gmail.com>
2023-08-16 19:34:01 -0500

    proyecto inicial de farmacia

---
+++ src/main/java/pe/edu/cibertec/farmacia/FarmaciaQueKiwiApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaQueKiwiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaQueKiwiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/farmacia/FarmaciaQueKiwiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaQueKiwiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
